bdbc3004f262d7d34bbbea58061dbdcdf4d4bf9d
Tanisha <tjmedi87@gmail.com>
2022-04-01 23:32:13 -0400

    correction for passing state variables

--- app.js
+++ app.js
@@ -13,18 +13,71 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 let api1, api2, api3 = "";
+let state1, state2, state3 = "";
 app.get('/', function(req, res) {
   let api1='none';
-  res.render('index.html' , { api1: api1, api2: api2, api3: api3 });
+  res.render('index.html' , { api1: api1, api2: api2, api3: api3, state1: state1, state2: state2, state3: state3 });
 });
 
 
 
 app.post('/', function(req, res) {
-  let obj ={"Alabama":"AL","Alaska":"AK","Connecticut":"CT"};
- let state1 = req.body.state1;
+
+  let obj ={
+    "Alabama":"AL",
+    "Alaska":"AK",
+    "Arizona":"AZ",
+    "Arkansas":"AR",
+    "California":"CA",
+    "Colorado":"CO",
+    "Connecticut":"CT",
+    "Delaware":"DE",
+    "Florida":"FL",
+    "Georgia":"GA",
+    "Hawaii":"HI",
+    "Idaho":"ID",
+    "Illinois":"IL",
+    "Indiana":"IN",
+    "Iowa":"IA",
+    "Kansas":"KS",
+    "Kentucky":"KY",
+    "Louisiana":"LA",
+    "Maine":"ME",
+    "Maryland":"MD",
+    "Massachusetts":"MA",
+    "Michigan":"MI",
+    "Minnesota":"MN",
+    "Mississippi":"MS",
+    "Missouri":"MO",
+    "Montana":"MT",
+    "Nebraska":"NE",
+    "Nevada":"NV",
+    "New Hampshire":"NH",
+    "New Jersey":"NJ",
+    "New Mexico":"NM",
+    "New York":"NY",
+    "North Carolina":"NC",
+    "North Dakota":"ND",
+    "Ohio":"OH",
+    "Oklahoma":"OK",
+    "Oregon":"OR",
+    "Pennsylvania":"PA",
+    "Rhode Island":"RI",
+    "South Carolina":"SC",
+    "South Dakota":"SD",
+    "Tennessee":"TN",
+    "Texas":"TX",
+    "Utah":"UT",
+    "Vermont":"VT",
+    "Virginia":"VA",
+    "Washington":"WA",
+    "West Virginia":"WV",
+    "Wisconsin":"WI",
+    "Wyoming":"WY"
+  };
+  let state1 = req.body.state1;
   let state2 = req.body.state2;
- let state3 = req.body.state3;
+  let state3 = req.body.state3;
 
   console.log('***'+state1);
 
